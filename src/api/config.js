2ed18c6558c6454b2fb885c735ad32fff78df87e
Carlos Ramirez <carlos@extrasistemas.com>
2018-03-08 09:51:50 -0600

    initial commit, width app platzimusic of course vue

---
+++ src/api/config.js
@@ -0,0 +1,13 @@
+/*
+https://www.last.fm/
+Application name	platzi_carlos
+API key	63522f6bf0b0864a69fa012f2f6c2db7
+Shared secret	e2239c51a95cd73c75c3cccfb6ee51f1
+Registered to	carlosextra1
+*/
+export default {
+	appName: 'platzi_carlos',
+	apiKey: '63522f6bf0b0864a69fa012f2f6c2db7',
+	secret: 'e2239c51a95cd73c75c3cccfb6ee51f1',
+	registeredTo: 'carlosextra1'
+};
