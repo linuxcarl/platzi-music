86b893a28b550b7ba63e8dd732c13970e72d153c
Carlos Ramirez <carlos@extrasistemas.com>
2018-03-08 10:09:51 -0600

    correccion de url de archivos para poder visualizar la app en github

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   #app
-    img(src='./assets/logo.png')
+    img(src='https://raw.githubusercontent.com/linuxcarl/platzi-music/master/dist/logo.png')
     h1 CarlosMiusic
     select(v-model="selectedCountry")
       option(v-for="country in countries" v-bind:value="country.value") {{country.name}}
